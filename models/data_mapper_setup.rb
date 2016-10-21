98b55599b35d3bc206d1aafee05f6aff0f484c42
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-21 15:53:24 +0100

    Rakefile added

--- models/data_mapper_setup.rb
+++ models/data_mapper_setup.rb
@@ -5,5 +5,4 @@ def configure
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
   DataMapper.finalize
-  DataMapper.auto_upgrade!
 end
